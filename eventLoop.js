63a32b94a12ca86d354796d9483df20eabcd5a26
Abdirizackmata <abdirizackissack2018@gmail.com>
2024-08-10 06:27:57 -0700

    covered eventloop

---
+++ eventLoop.js
@@ -0,0 +1,12 @@
+// console.log("conslole log 1");
+// process.nextTick(() => {
+//   console.log("this is process.nextTick 1");
+// });
+// console.log("conslole log 2");
+
+// Promise.resolve().then(() => {
+//   console.log("this is promise.resolve 1");
+// });
+// process.nextTick(() => {
+//   console.log("this is process.nextTick 1");
+// });
