e5866ce74558728b7104b59e44d060f7bf7b7b1d
Abdirizackmata <abdirizackissack2018@gmail.com>
2024-09-19 06:07:37 -0700

    covered setImmediate and check que in event lope

--- ioQue.js
+++ ioQue.js
@@ -1,21 +1,3 @@
-const fs = require("node:fs");
-
-fs.readFile(__filename, () => {
-  console.log("This is readFile 1");
-});
-
-process.nextTick(() => {
-  console.log("This is process.nextTick 1");
-});
-
-Promise.resolve().then(() => {
-  console.log("This is promise.reslove 1");
-});
-
-setTimeout(() => {
-  console.log("This is setTimeout 1");
-}, 0);
-setImmediate(() => console.log("This is setTimeout 1"));
-
-
-for (let i = 0; i < 2000000000; i++) {}
+setTimeout(() => console.log("This is setTimeout 1"), 0);
+setImmediate(() => console.log("This is setImmediate 1"));
+  
